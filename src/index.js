db51d3a0feeb54ffe01c590e481d4155e057e750
Sean Baskin <seanbaskin@gmail.com>
2019-03-25 14:34:38 -0400

    Lots of work getting ahead of myself

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from "react";
-import ReactPDF from '@react-pdf/renderer';
-import Resume from "./Resume.js";
+import React from 'react';
+import ReactPDF, { Font } from '@react-pdf/renderer';
+import Resume from './components/Resume.js';
+
+// Register fonts
+Font.register(`${__dirname}/fonts/FiraCode-Regular.woff`, {
+  family: 'Fira Code',
+});
 
 ReactPDF.render(<Resume />, `${__dirname}/resume.pdf`);
