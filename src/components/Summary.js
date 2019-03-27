e8892fde507430dc2e4fdfa7024a0705b18ce847
Sean Baskin <seanbaskin@gmail.com>
2019-03-27 15:43:44 -0400

    Refactored the summary text

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -15,18 +15,18 @@ const styles = StyleSheet.create({
   }
 });
 
-const summaryText = "I develop enterprise Java and Spring web applications and \
-am passionate about the work I engage in. That passion has driven varied \
-interests that I have incorporated into my professional portfolio ranging from \
-the core Amazon Web Services product offerings, the Docker platform, and a \
-healthy dose of DevOps tools and practices. Goal-oriented with a focus on the \
-entire software development cycle. Oh, I also like maps and mathematics.";
+const summaryItems = [
+  "I develop enterprise Java and Spring web applications and am passionate about the work I engage in.",
+  "That passion has driven varied interests that I have incorporated into my professional portfolio ranging from the core Amazon Web Services product offerings, the Docker platform, and a healthy dose of DevOps tools and practices.",
+  "Goal-oriented with a focus on the entire software development cycle.",
+  "Oh, I also like maps, mathematics, and other programming languages."
+];
 
 // Create Document Component
 const Summary = () => (
   <View style={styles.summaryContainer}>
     <Text style={styles.header}>Summary</Text>
-    <Text>{summaryText}</Text>
+    <Text>{summaryItems.join(' ')}</Text>
   </View>
 );
 
