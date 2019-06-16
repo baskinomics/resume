a2cbe3bf8d6c0fe76b3bab5ce6ce25d7fde96719
Sean Baskin <seanbaskin@gmail.com>
2019-06-16 16:37:19 -0400

    Added icons to meta links

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,50 +34,79 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     fontSize: '9pt',
     fontFamily: 'Open Sans Condensed Bold'
+  },
+  icon: {
+    fontFamily: 'Hasklug',
+    fontSize: 12,
+    marginRight: '5px',
+  },
+  metaLinks: {
+    display: 'flex',
+    flexDirection: 'row',
   }
 });
 
 const metaLinks = [
   {
     id: 0,
+    icon: "\uf83b",
     text: "LinkedIn",
     href: "https://www.linkedin.com/in/sean-baskin/"
   },
   {
     id: 1,
+    icon: "\ue709",
     text: "GitHub",
     href: "https://github.com/baskinomics"
   },
   {
     id: 2,
+    icon: "\ufb0a",
     text: "StackExchange",
     href: "https://stackexchange.com/users/944752/baskinomics"
   },
   {
     id: 3,
+    icon: "\uf6ef",
     text: "seanbaskin@gmail.com",
     href: "mailto:seanbaskin@gmail.com"
   },
   {
     id: 4,
+    icon: "\uf8f1",
     text: "(423) 580-9185",
     href: "tel:1-423-580-9185"
   }
 ];
 
+// todo documentation
 const Name = () => (
-  <View style={styles.nameContainer}>
+  <View style={ styles.nameContainer }>
     <Text>Sean</Text>
     <Text>Baskin</Text>
   </View>
 );
 
+// todo documentation
+const Icon = ({ unicode }) => (
+  <Text style={ styles.icon }>{ unicode }</Text>
+);
+
+// todo documentation
+const MetaLink = ({ link }) => (
+  <View style={ styles.metaLinks }>
+    <Icon unicode={ link.icon }/>
+    <Link src={ link.href }>{ link.text }</Link>
+  </View>
+);
+
+// todo documentation
 const Meta = ({ metaLinks }) => (
-  <View style={styles.metaContainer}>
+  <View style={ styles.metaContainer }>
     {
       // Per https://reactjs.org/docs/lists-and-keys.html#keys
       metaLinks.map(link => (
-        <Link key={link.id} src={link.href}>{ link.text }</Link>
+        <MetaLink key={ link.id } link={ link }/>
       ))
     }
   </View>
@@ -85,9 +114,9 @@ const Meta = ({ metaLinks }) => (
 
 // Create Document Component
 const Header = () => (
-  <View style={styles.headerContainer}>
+  <View style={ styles.headerContainer }>
     <Name />
-    <Meta metaLinks={metaLinks} />
+    <Meta metaLinks={ metaLinks } />
   </View>
 );
 
