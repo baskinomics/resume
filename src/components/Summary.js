64bd7f185ddb6fe20d25238cc4c00ff7c01e3913
Sean Baskin <seanbaskin@gmail.com>
2019-07-10 17:31:18 -0400

    Updated summary content

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -16,10 +16,13 @@ const styles = StyleSheet.create({
 });
 
 const summaryItems = [
-  "I develop enterprise Java and Spring web applications and am passionate about the work I engage in.",
-  "That passion has driven varied interests that I have incorporated into my professional portfolio ranging from the core Amazon Web Services to a healthy dose of DevOps tools and practices.",
-  "Goal-oriented with a focus on the entire software development cycle.",
-  "I live in Linux and I also like maps, mathematics, and other programming languages!"
+  "Experienced software engineer and consultant with a background in computer science, geographic information science, and mathematics.",
+  // "I develop enterprise Java and Spring web applications and am passionate about the work I engage in.",
+  // "That passion has driven varied interests that I have incorporated into my professional portfolio ranging from the core Amazon Web Services to a healthy dose of DevOps tools and practices.",
+  "Creative yet pragmatic in development and implementation, as well as problem resolution.",
+  "Excellent written and oral communication skills and interpersonal dynamics, with proven ability to effectively use those skills to ensure transparency and agreement between technical and non-technical stakeholders.",
+  "Engaged and knowledgeable individual that is goal-oriented, proactive, and effectual.",
+  // "I live in Linux and I also like maps, mathematics, and other programming languages!"
 ];
 
 // Create Document Component
