40386ea837f0eff27be9ce0828836fc7a373e3cd
Sean Baskin <seanbaskin@gmail.com>
2019-07-14 17:41:15 -0400

    Fleshed out work experience data

--- src/data/Experience.js
+++ src/data/Experience.js
@@ -29,8 +29,173 @@ export const experiencePageOne = [
       end: "April 2014"
     },
     summary: [
-      {id: 0, text: "Evaluated available data portal solutions and deployed and managed chosen solution."},
-      {id: 1, text: "Coordinated and consulted with the Mayor's Office and various departments to identify and disseminate government data to the public."}
+      {id: 0, text: "Coordinated and consulted with the Mayor's Office and various departments to identify and disseminate government data to the public."},
+      {id: 1, text: "Evaluated available data portal solutions and managed chosen platform."},
     ]
   }
+];
+
+export const experiencePageTwo = [
+  {
+    id: 0,
+    title: "Consultant: Self-Employed",
+    organization: "Etowah River Trail",
+    dates: {
+      begin: "Jan. 2014",
+      end: "Feb. 2014"
+    },
+    // todo should we add link? http://www.etowahwatertrail.org/
+    summary: [
+      {id: 0, text: "Configured and deployed WordPress website for client."},
+      {id: 1, text: "Redesigned front-end WordPress template to client's specifications."},
+      {id: 2, text: "Produced thematic maps for one hundred seventy (170) miles of the Etowah River Trail."}
+    ]
+  },
+  {
+    id: 1,
+    title: "Graduate Research Assistant",
+    organization: "University of Tennessee at Chattanooga",
+    dates: {
+      begin: "May 2013",
+      end: "Oct. 2013"
+    },
+    summary: [
+      {id: 0, text: "Contributed to the development of a distributed geospatial web application within the context of the Thrive 2055 initiative."},
+      {id: 1, text: "Additionally, developed an Android application for the collection of WiFi signal strength and location data for the campus of the University of Tennessee at Chattanooga."}
+    ]
+  },
+  {
+    id: 2,
+    title: "Software Developer",
+    organization: "SecondSite, LLC.",
+    dates: {
+      begin: "May 2012",
+      end: "Aug. 2012"
+    },
+    summary: [
+      {id: 0, text: "Contribubted to the development of an augmented reality (AR) mobile application for the Smithsonian National Museum of African American History and Culture."},
+      {id: 1, text: "Contributed to the development of an augmented reality (AR) mobile application that provided an innovative system for the maintenance of art and artifacts."}
+    ]
+  },
+  {
+    id: 3,
+    title: "GIS Consultant: Self-Employed",
+    organization: "SecondSite, LLC.",
+    dates: {
+      begin: "Feb. 2012",
+      end: "April 2012"
+    },
+    summary: [
+      {id: 0, text: "Provided consulting services to SecondSite, LLC and the SimCenter Enterprise for Disaster Mitigation Plume/Evacuation Scenario Modeling as part of the U.S. Ignite Initiative"},
+    ]
+  },
+  {
+    id: 4,
+    title: "Graduate Teaching Assistant",
+    organization: "University of Tennessee at Chattanooga",
+    dates: {
+      begin: "Jan. 2012",
+      end: "May 2012"
+    },
+    summary: [
+      {id: 0, text: "Graded papers, assisted in labs, proctored exams, and on occasion stood in for my professor during class lectures."},
+    ]
+  },
+  {
+    id: 5,
+    title: "Graduate Research Assistant",
+    organization: "University of Tennessee at Chattanooga",
+    dates: {
+      begin: "Jan. 2012",
+      end: "May 2012"
+    },
+    summary: [
+      {id: 0, text: "Conducted self-directed research on mobile indoor navigation systems using WiFi signal strength, Gaussian processes, cloud computing, the Android framework, and the R language."},
+    ]
+  },
+  {
+    id: 6,
+    title: "Web GIS Developer",
+    // todo consider wrapping this to two lines
+    organization: "Academic Research and Computing Services - UTC",
+    dates: {
+      begin: "June 2011",
+      end: "Dec. 2012"
+    },
+    summary: [
+      {id: 0, text: "Assisted in the deployment and management of ArcGIS Server 10 and ArcSDE databases."},
+      {id: 1, text: "Collected, documented, and organized spatial information for a sixteen (16) county region."},
+      {id: 2, text: "Developed RESTful geoprocessing tasks and implemented ArcGIS API for Flex web application to consume them."}
+    ]
+  },
+  {
+    id: 7,
+    title: "Intern",
+    organization: "Tennessee Valley Authority",
+    dates: {
+      begin: "July 2010",
+      end: "Aug. 2011"
+    },
+    summary: [
+      {id: 0, text: "Designed and implemented enterprise spatial database of bathymetric point data for TVA’s reservoir system, including data management, documentation and metadata, and the production of TIN and raster models."},
+      {id: 1, text: "Upon request, I fulfilled client tasks pertaining to the data such as cross-section analysis and spatial queries."}
+    ]
+  },
+  {
+    id: 8,
+    title: "Math Tutor",
+    // todo implemented component for department/org
+    // UTK Math Tutorial Center
+    organization: "University of Tennessee at Knoxville",
+    dates: {
+      begin: "Aug. 2008",
+      end: "May 2010"
+    },
+    summary: [
+      {id: 0, text: "Tutored students at the UTK Math Tutorial Center on material ranging from Calculus I to Calculus II and proctored exams."},
+    ]
+  },
+  {
+    id: 9,
+    title: "Lifeguard",
+    organization: "Sportsbarn",
+    dates: {
+      begin: "May 2008",
+      end: "Aug. 2008"
+    },
+    summary: []
+  },
+  {
+    id: 10,
+    title: "Lifeguard",
+    organization: "YMCA",
+    dates: {
+      begin: "May 2006",
+      end: "Aug. 2007"
+    },
+    summary: []
+  },
+  {
+    id: 11,
+    title: "Cashier",
+    organization: "Carmike Cinemas",
+    dates: {
+      begin: "May 2005",
+      end: "Aug. 2005"
+    },
+    summary: []
+  },
+  {
+    id: 12,
+    title: "Founder and Owner",
+    organization: "LoserLabeled.com",
+    dates: {
+      begin: "Jan. 2003",
+      end: "Dec. 2005"
+    },
+    summary: [
+      {id: 0, text: "Loserlabeled.com was a website with a focus on skateboarding that offered original articles, tutorials, media, and a vibrant user community."},
+      {id: 1, text: "It was designed and developed with two partners that utilized search engine optimization (SEO) techniques and Google Adsense program to generate profitable advertising revenue."}
+    ]
+  },
 ];
