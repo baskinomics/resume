d9b4c57929600fa51ed884b719b42908a38dda58
Sean Baskin <seanbaskin@gmail.com>
2019-09-30 07:42:17 -0400

    Added margins and padding back

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -6,10 +6,10 @@ import { COLUMN_WIDTH } from '../Attributes.js';
 const styles = StyleSheet.create({
   summaryContainer: {
     width: `${COLUMN_WIDTH}px`,
-    // marginTop: "3px",
-    // marginBottom: "5px",
-    // paddingLeft: '5px',
-    // paddingRight: '5px'
+    marginTop: "5px",
+    marginBottom: "5px",
+    paddingLeft: "5px",
+    paddingRight: "5px"
   },
 });
 
@@ -18,12 +18,11 @@ const summaryItems = [
   "Creative yet pragmatic in development and implementation, as well as problem resolution.",
   "Excellent written and oral communication skills and interpersonal dynamics, with proven ability to effectively use those skills to ensure transparency and agreement between technical and non-technical stakeholders.",
   "Engaged and knowledgeable individual that is goal-oriented, proactive, and effectual.",
-  ""
 ];
 
 // Declare Summary Component
 const Summary = () => (
-  <Text style={styles.summaryContainer} debug={false}>{summaryItems.join(' ')}</Text>
+  <Text style={styles.summaryContainer} debug={true}>{summaryItems.join(' ')}</Text>
 );
 
 export default Summary;
