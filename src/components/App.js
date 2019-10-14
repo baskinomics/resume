502f7794d56dc74740e56c87dbf19114857d9668
Sean Baskin <seanbaskin@gmail.com>
2019-10-14 06:41:05 -0400

    Changed styled component

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Resume = styled.p`
+const Resume = styled.div`
     font-family: "Source Sans Pro";
+    font-size: 16px;
 `;
 
 class App extends React.Component {
